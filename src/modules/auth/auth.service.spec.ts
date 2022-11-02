756d16822729b3e67680670cffd1f7d4673aaa20
yisnur <yisnur23@gmail.com>
2022-11-02 20:48:33 +0300

    add auth service test

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,18 +1,74 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Profile } from 'passport-google-oauth20';
+import { User } from '../profile/entities/user.entity';
+import { ProfileService } from '../profile/profile.service';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
-
+  let profileService: ProfileService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: ProfileService,
+          useValue: {
+            findUserByEmail: jest.fn(),
+            createUser: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    profileService = module.get<ProfileService>(ProfileService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+  describe('findOrCreateUser service', () => {
+    it('returns a user if it exists', async () => {
+      const user = {
+        id: 'user_id',
+      } as User;
+      const email = 'email';
+      jest.spyOn(profileService, 'findUserByEmail').mockResolvedValue(user);
+
+      const profile = {
+        emails: [
+          {
+            value: email,
+          },
+        ],
+      } as Profile;
+      const returnedValue = await service.findOrCreateUser(profile);
+
+      expect(profileService.findUserByEmail).toBeCalledTimes(1);
+      expect(profileService.findUserByEmail).toBeCalledWith(email);
+      expect(returnedValue).toEqual(user);
+    });
+    it('creates and returns a user if it doesnot exist', async () => {
+      const email = 'email';
+      const profile = {
+        emails: [
+          {
+            value: email,
+          },
+        ],
+        photos: [
+          {
+            value: 'value',
+          },
+        ],
+      } as Profile;
+      jest.spyOn(profileService, 'findUserByEmail');
+      jest.spyOn(profileService, 'createUser');
+
+      await service.findOrCreateUser(profile);
+      expect(profileService.findUserByEmail).toBeCalledTimes(1);
+      expect(profileService.createUser).toBeCalledTimes(1);
+    });
   });
 });
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -6,16 +6,16 @@ import { ProfileService } from '../profile/profile.service';
 export class AuthService {
   constructor(private profileService: ProfileService) {}
 
-  findOrCreateUser(profile: Profile) {
-    const email = profile.emails[0].value;
-    let user = this.profileService.findUserByEmail(email);
+  async findOrCreateUser(profile: Profile) {
+    const email = profile?.emails[0]?.value;
+    let user = await this.profileService.findUserByEmail(email);
     if (!user) {
-      user = this.profileService.createUser({
+      user = await this.profileService.createUser({
         email,
         display_name: profile.displayName,
-        first_name: profile?.name.givenName,
-        last_name: profile?.name.familyName,
-        profile_picture: profile.photos[0].value,
+        first_name: profile?.name?.givenName,
+        last_name: profile?.name?.familyName,
+        profile_picture: profile?.photos[0]?.value,
       });
     }
     return user;
